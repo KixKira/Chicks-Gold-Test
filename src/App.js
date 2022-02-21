dae332fda882294d81632db4f2d8ed379238ed17
Oscar Machado <oemqkix@gmail.com>
2022-02-20 21:01:01 -0300

    Importanto componente FormContainer

--- src/App.js
+++ src/App.js
@@ -1,66 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import FormContainer from "./components/form-container";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* Inputs */}
-        <div className="container">
-          <div className="row align-items-start">
-            <div className="col">
-              <div className="row g-3 align-items-center">
-                <div className="col-auto">
-                  <label className="col-form-label">Bucket X</label>
-                </div>
-                <div className="col-auto">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Input value"
-                    pattern="[0-9]*"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="row g-3 align-items-center">
-                <div className="col-auto">
-                  <label className="col-form-label">Bucket Y</label>
-                </div>
-                <div className="col-auto">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Input value"
-                    pattern="[0-9]*"
-                  />
-                </div>
-                <div className="row mb-1 align-items-center">
-                  <button type="submit" className="btn btn-primary">
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="row g-3 align-items-center">
-                <div className="col-auto">
-                  <label className="col-form-label">Bucket Z</label>
-                </div>
-                <div className="col-auto">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Input value"
-                    pattern="[0-9]*"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+        <div>
+          <FormContainer />
         </div>
-        <br></br>
 
         {/*Buckets*/}
         <div className="container">
